3f2cb8952613db9ae50eb13998e60fdc415f1b46
ggaleanoguerra <ggaleanoguerra@correo.unicordoba.edu.co>
2024-07-19 13:05:42 -0500

    refactor: Update Spotify authentication endpoints and remove unused code

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,29 +5,58 @@ const router = Router();
 
 const clientId = process.env.CLIENT_ID;
 const clientSecret = process.env.CLIENT_SECRET;
+const redirectUri = process.env.REDIRECT_URI;
 
 router.get('/auth/token', async (req, res) => {
-    const tokenUrl = 'https://accounts.spotify.com/api/token';
-    const authOptions = {
-        method: 'POST',
-        headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Authorization': 'Basic ' + Buffer.from(clientId + ':' + clientSecret).toString('base64'),
-        },
-        data: qs.stringify({
-        grant_type: 'client_credentials',
-        }),
-        url: tokenUrl,
-    };
-    
-    try {
-        const response = await axios(authOptions);
-        res.json(response.data);
-    } catch (error) {
-        console.error('Error fetching token:', error.response ? error.response.data : error.message);
-        res.status(500).json({ error: 'Failed to fetch token', details: error.response ? error.response.data : error.message });
-    }
-    }
-);
+  const tokenUrl = 'https://accounts.spotify.com/api/token';
+  const authOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Authorization': 'Basic ' + Buffer.from(clientId + ':' + clientSecret).toString('base64'),
+    },
+    data: qs.stringify({
+      grant_type: 'client_credentials',
+    }),
+    url: tokenUrl,
+  };
+
+  try {
+    const response = await axios(authOptions);
+    res.json(response.data);
+  } catch (error) {
+    console.error('Error fetching token:', error.response ? error.response.data : error.message);
+    res.status(500).json({
+      error: 'Failed to fetch token',
+      details: error.response ? error.response.data : error.message,
+    });
+  }
+});
+
+router.post('/auth/callback', async (req, res) => {
+  const code = req.body.code;
+  const tokenUrl = 'https://accounts.spotify.com/api/token';
+  const authOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Authorization': 'Basic ' + Buffer.from(clientId + ':' + clientSecret).toString('base64'),
+    },
+    data: qs.stringify({
+      grant_type: 'authorization_code',
+      code: code,
+      redirect_uri: redirectUri,
+    }),
+    url: tokenUrl,
+  };
+
+  try {
+    const response = await axios(authOptions);
+    res.json(response.data);
+  } catch (error) {
+    console.error('Error during Spotify callback:', error.response ? error.response.data : error.message);
+    res.status(500).json({ error: 'Failed to exchange code for token', details: error.response ? error.response.data : error.message });
+  }
+});
 
 module.exports = router;
